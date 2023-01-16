94c7817e5558f7fb1d78d525b2397e17f3fdf081
GeovaneDockhorn <geovanedockhorn2015@gmail.com>
2023-01-16 17:59:26 -0300

    excecao para cpf e email já cadastrados

--- src/main/java/com/geovane/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/geovane/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.geovane.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.geovane.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -21,4 +22,16 @@ public class ResourceExceptionHandler {
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+			HttpServletRequest request) {
+
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"DataIntegrityViolationException", ex.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
 }
+
+
--- src/main/java/com/geovane/helpdesk/services/TecnicoService.java
+++ src/main/java/com/geovane/helpdesk/services/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.geovane.helpdesk.domain.Pessoa;
 import com.geovane.helpdesk.domain.Tecnico;
 import com.geovane.helpdesk.domain.dtos.TecnicoDTO;
+import com.geovane.helpdesk.repositories.PessoaRepository;
 import com.geovane.helpdesk.repositories.TecnicoRepository;
+import com.geovane.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.geovane.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,6 +19,9 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository tecnicoRepository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> optionalTecnico = tecnicoRepository.findById(id);
@@ -28,8 +34,23 @@ public class TecnicoService {
 	
 	public Tecnico create(TecnicoDTO tecnicoDTO) {
 		tecnicoDTO.setId(null);
+		validaPorCpfEEmail(tecnicoDTO);
 		Tecnico tecnico = new Tecnico(tecnicoDTO);
 		return tecnicoRepository.save(tecnico);
+	}
+	
+	private void validaPorCpfEEmail(TecnicoDTO tecnicoDTO) {
+		
+		Optional<Pessoa> optionalPessoa = pessoaRepository.findByCpf(tecnicoDTO.getCpf());
+		if(optionalPessoa.isPresent() && optionalPessoa.get().getId() != tecnicoDTO.getId()) {
+			throw new DataIntegrityViolationException("Cpf já cadastrado no sistema!");
+		}
+		
+		optionalPessoa = pessoaRepository.findByEmail(tecnicoDTO.getEmail());
+		if(optionalPessoa.isPresent() && optionalPessoa.get().getId() != tecnicoDTO.getId()) {
+			throw new DataIntegrityViolationException("Email já cadastrado no sistema!");
+		}
+		
 	}
 	
 }
